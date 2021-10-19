bf3e99fd70c693a1bfa3a76dad686fd3828decdc
Konstantin Velkov <konstantinmvelkov@gmail.com>
2021-10-19 12:22:20 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,10 +6,9 @@ echo $SCHEME
 echo $DESTINATION
 echo $PROJECT
 
-run: |
-    arch -arm64 xcodebuild clean test \
-    -project $PROJECT \
-    -scheme $SCHEME \
-    -destination $DESTINATION \
-    -resultBundlePath "Build/Result/${GITHUB_RUN_ID}-iOS15-Simulator.xcresult" \
-    -derivedDataPath Build/DerivedData | xcpretty && exit ${PIPESTATUS[0]}
+arch -arm64 xcodebuild clean test \
+     -project $PROJECT \
+     -scheme $SCHEME \
+     -destination $DESTINATION \
+     -resultBundlePath "Build/Result/${GITHUB_RUN_ID}-iOS15-Simulator.xcresult" \
+     -derivedDataPath Build/DerivedData | xcpretty && exit ${PIPESTATUS[0]}
